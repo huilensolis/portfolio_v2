3eacd99fced4584d97f32cd183220edeea3a8fde
Huilensolis <xywvanz@gmail.com>
2023-10-01 17:17:05 -0300

    theme switcher fixed

--- src/app/components/theme-switcher.tsx
+++ src/app/components/theme-switcher.tsx
@@ -7,8 +7,7 @@ import { useEffect, useState } from "react";
 export function ThemeSwitcher() {
   const [isToggled, setToggle] = useState(() => {
     if (typeof localStorage !== "undefined") {
-      const initialValue = (localStorage.theme = "light" ? true : false);
-      return initialValue;
+      return localStorage.theme === "light";
     }
     return false;
   });
@@ -17,14 +16,14 @@ export function ThemeSwitcher() {
     if (typeof localStorage !== "undefined") {
       if (
         localStorage.theme === "dark" ||
-        (!("theme" in localStorage) &&
+        (!localStorage.theme &&
           window.matchMedia("(prefers-color-scheme: dark)").matches)
       ) {
         document.documentElement.classList.add("dark");
-        localStorage.theme === "dark";
+        localStorage.theme = "dark";
       } else {
         document.documentElement.classList.remove("dark");
-        localStorage.theme === "light";
+        localStorage.theme = "light";
       }
     }
   }, [isToggled]);
@@ -34,8 +33,8 @@ export function ThemeSwitcher() {
       localStorage.theme === "dark"
         ? (localStorage.theme = "light")
         : (localStorage.theme = "dark");
-      setToggle(!isToggled);
     }
+    setToggle((prevState) => !prevState);
   }
 
   return (
