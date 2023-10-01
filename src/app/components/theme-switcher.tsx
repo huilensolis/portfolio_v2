85cdb4466004d76967c3fd53a7507df2f0441e2e
Huilensolis <xywvanz@gmail.com>
2023-10-01 17:00:04 -0300

    fix local storage not defined errors

--- src/app/components/theme-switcher.tsx
+++ src/app/components/theme-switcher.tsx
@@ -6,29 +6,36 @@ import { useEffect, useState } from "react";
 
 export function ThemeSwitcher() {
   const [isToggled, setToggle] = useState(() => {
-    const initialValue = (localStorage.theme = "light" ? true : false);
-    return initialValue;
+    if (typeof localStorage !== "undefined") {
+      const initialValue = (localStorage.theme = "light" ? true : false);
+      return initialValue;
+    }
+    return false;
   });
 
   useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-      localStorage.theme === "dark";
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme === "light";
+    if (typeof localStorage !== "undefined") {
+      if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        document.documentElement.classList.add("dark");
+        localStorage.theme === "dark";
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.theme === "light";
+      }
     }
   }, [isToggled]);
 
   function handleToggle() {
-    localStorage.theme === "dark"
-      ? (localStorage.theme = "light")
-      : (localStorage.theme = "dark");
-    setToggle(!isToggled);
+    if (typeof localStorage !== "undefined") {
+      localStorage.theme === "dark"
+        ? (localStorage.theme = "light")
+        : (localStorage.theme = "dark");
+      setToggle(!isToggled);
+    }
   }
 
   return (
