9ce33bbb6b7e401381cb02d13e6f70ced2a97822
Huilensolis <xywvanz@gmail.com>
2023-10-08 00:23:47 -0300

    create metadata for blog page and portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import type { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `Huilen Solis's Portfolio`,
+    description:
+      "Full stack developer, Working with React, Nextjs, Nodejs and Tailwind!",
+    openGraph: {
+      images: [{ url: "../../public/icon.png", alt: "Huilen Solis logo" }],
+      title: `Huilen Solis's Portfolio`,
+      description:
+        "Full stack developer, Working with React, Nextjs, Nodejs and Tailwind!",
+      type: "website",
+      siteName: "Huilen Solis's Portfolio",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `Huilen Solis's Portfolio`,
+      description:
+        "Full stack developer, Working with React, Nextjs, Nodejs and Tailwind!",
+      creator: "@solishuilen",
+      images: [{ url: "../../public/icon.png", alt: "Huilen Solis logo" }],
+    },
+  };
+}
+
 export default function Home() {
   return (
     <main className="h-screen w-full flex justify-start items-start">
