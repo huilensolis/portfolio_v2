0735ad83ce6d44ed5cc31a779dc274045ad85d5e
Huilensolis <xywvanz@gmail.com>
2023-12-20 21:58:41 -0300

    fix: fix heading classname

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
       <SquarePatten />
       <section className="flex flex-col items-center gap-24 max-w-5xl w-full relative">
         <main className="flex flex-col items-center px-20 pb-20 cm-1xl:p-4 justify-center z-[3]">
-          <h1 className="text-5xl cm-lg:text-4xl break-words font-bold text-neutral-300 text-center z-10 flex justify-center items-center [text-wrap:_center]">
+          <h1 className="text-5xl cm-lg:text-4xl break-words font-bold text-neutral-300 text-center z-10 flex justify-center items-center [text-wrap:_balance]">
             Frontend Engineer - Forging Solutions with The-State-Of-Art
             Technologies, Enforcing Accessibility for Inclusive User Experiences
           </h1>
